9107a4a2ed5aa0c1c2de7fef44e80b961d576a19
Michael JasonSmith <mpj17@onlinegroups.net>
2010-07-28 14:44:00 +1000

    Adding the core of the Invitation Withdrawn page.
    --HG--
    rename : gs/profile/invite/browser/templates/invitation-responded.pt => gs/profile/invite/browser/templates/invitation-withdrawn.pt

--- gs/profile/invite/invitationredirect.py
+++ gs/profile/invite/invitationredirect.py
@@ -7,13 +7,13 @@ from invitation import Invitation
 
 class GSInvitationResponseRedirect(GSRedirectBase):
     def __call__(self):
-        # TODO: Audit
+        
         if len(self.traverse_subpath) == 1:
             invitationId = self.traverse_subpath[0]
             
             if (invitationId == 'example'):
                 userInfo = createObject('groupserver.LoggedInUser',
-                                        self.context)
+                                        self.context.aq_self)
                 ref = self.request.get('HTTP_REFERER','')
                 assert ref, 'This page only works if you follow the link '\
                     'from the invitation preview.'
@@ -22,20 +22,23 @@ class GSInvitationResponseRedirect(GSRedirectBase):
                 uri = '%s/intial_response.html?form.invitationId=example&form.groupId=%s' %\
                   (userInfo.url, groupId)
             else: # Not an example
-                invitation = Invitation(self.context, invitationId)
+                invitation = Invitation(self.context.aq_self, invitationId)
                 try:
-                    hadResponse = invitation.invite['response_date'] !=  None
+                    hadResponse = \
+                        invitation.invite['response_date'] !=  None
+                    invitationWithdrawn = \
+                        invitation.invite['withdrawn_date'] !=  None
                 except KeyError, err:
-                    hadResponse = False
-                    inviteExists = False
+                    hadResponse = inviteExists = invitationWithdrawn = False
                 else:
                     inviteExists = True
 
                 if inviteExists and hadResponse:
                     uri = '/invitation-responded.html?form.invitationId=%s' %\
-                        invitationId
-                elif inviteExists and not(hadResponse):
-                    login(self.context, invitation.userInfo.user)
+                            invitationId
+                elif inviteExists and not(hadResponse) and not(invitationWithdrawn):
+                    # --=mpj17=-- Only ever log in when responding
+                    login(self.context.aq_self, invitation.userInfo.user)
                     if invitation.invite['initial_invite']:
                         # Go to the initial response page, so
                         #   the new user can set a password (and verify
@@ -48,9 +51,13 @@ class GSInvitationResponseRedirect(GSRedirectBase):
                         #   the normal Response page.
                         uri = '%s/invitations_respond.html' % \
                                 invitation.userInfo.url
+                elif inviteExists and invitationWithdrawn:
+                    # The invitation has been withdrawn
+                    uri = '/invitation-withdrawn.html?form.invitationId=%s' %\
+                            invitationId
                 else: # Invitation does not exist
                     uri = '/invite-not-found.html?form.invitationId=%s' %\
-                        invitationId
+                            invitationId
         else: # Verification ID not specified
             uri = '/invite-no-id.html'
         assert uri
