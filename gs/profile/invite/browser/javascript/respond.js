732b1d42dff149931fb82a989dbe78d97c0dadb1
Michael JasonSmith <mpj17@onlinegroups.net>
2013-06-26 17:17:25 -0700

    Adding a password toggle.

--- gs/profile/invite/browser/javascript/respond.js
+++ gs/profile/invite/browser/javascript/respond.js
@@ -1,6 +1,12 @@
 jQuery.noConflict();
 
-function gs_profile_invite_respond_init() {
+function gs_profile_invite_respond_password_init () {
+    var toggler = null;
+    toggler = GSProfilePasswordToggle('#form\\.password1', 
+                                      '#gs-profile-invite-respond-password-set-toggle-widget');
+}
+
+function gs_profile_invite_respond_init () {
     jQuery("#form\\.password1").focus();
 
     jQuery("#privacy-button").click( function () {
@@ -12,6 +18,8 @@ function gs_profile_invite_respond_init() {
         var uri = "/policies/aup/ #aup";
         jQuery("#tc-content").load(uri);
     });
+    gsJsLoader.with_module('/++resource++gs-profile-password-toggle-min-20130516.js',
+                           gs_profile_invite_respond_password_init);            
 }
 
 jQuery(window).load( gs_profile_invite_respond_init );
